e9cc6b32de95592b11b986a632f8d357ad25cf27
mccartnp <nicholasmccarty252@gmail.com>
2023-11-23 16:38:36 -0500

    Board is layed out

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Chessboard from './components/Chessboard';
+
+function App() {
+  return (
+    <div id = "app">
+     <Chessboard/>
+    </div>
+  );
+}
+
+export default App;
